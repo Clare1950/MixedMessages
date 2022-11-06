ba8a2a6d2bf8f9cec29b8125bd7f421e90eb69d5
Clare Parkes <clare.parkes@gmail.com>
2022-11-06 20:51:01 +0000

    Added factory functions for the podcasts and series objects

--- script.js
+++ script.js
@@ -16,18 +16,66 @@ const bookFactory = (title, author, genre, pages) => {
       }
     },
     bookSummary() {
-      return `read ${this.title}, a ${this.genre} book by ${
+      return `Read ${this.title}, a ${this.genre} book by ${
         this.author
       }. \nIt is a ${this.length()} read with ${this.pages} pages.`;
     },
   };
 };
 
-let newBook = bookFactory(
+// Podcast Factory Function
+const podcastFactory = (title, creator, platform, episodes, episodeLength) => {
+  return {
+    title: title,
+    creator: creator,
+    platform: platform,
+    episodes: episodes,
+    episodeLength: episodeLength,
+    totalTime() {
+      return this.episodes * this.episodeLength;
+    },
+    podcastSummary() {
+      return `Listen to ${this.title}, created by ${
+        this.creator
+      }. \nIt is a ${this.totalTime()} minute total listen and you can find it on ${
+        this.platform
+      }.`;
+    },
+  };
+};
+
+// series factory function
+const seriesFactory = (title, genre, platform, episodes, episodeLength) => {
+  return {
+    title: title,
+    genre: genre,
+    platform: platform,
+    episodes: episodes,
+    episodeLength: episodeLength,
+    totalTime() {
+      return this.episodes * this.episodeLength;
+    },
+    seriesSummary() {
+      return `Watch ${this.title}, a ${this.genre} series you can find on ${
+        this.platform
+      }. \n${this.title} has a ${this.totalTime()} minute total watch time.`;
+    },
+  };
+};
+
+
+let book1 = bookFactory(
   "Slime: A Natural History",
   "Susanne Wedlich",
-  "popular science",
+  "Popular Science",
   272
 );
+let podcast1 = podcastFactory(
+  "The Trojan Horse Affair",
+  "Brain Reed & Hamza Syed",
+  "Spotify",
+  8,
+  50
+);
+let series1 = seriesFactory("Crossfire", "Drama", "BBC iPLayer", 3, 55);
 
-console.log(newBook.bookSummary());
